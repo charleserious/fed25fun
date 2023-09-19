a3ee8267d277ce4678a8ed896508577a8424c18e
The Keyboard Operator <jinjing.wu@compass.com>
2023-09-19 20:31:41 +0200

    fix: more fun

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 (() => {
-  document.body.style.backgroundImage = `url("https://source.unsplash.com/random?v=${Date.now()}")`
+  document.body.style.backgroundImage = `url(https://source.unsplash.com/random?v=${Date.now()})`
 })()
